b093c518df49e6488fb7dd5c83f952e91f567ad4
ethanholleman <etholleman@ucdavis.edu>
2021-02-05 11:49:13 -0800

    Start converting dictionary tables to dataframes in order to handle non A -> B coding tables

--- pyBridgeInv/__init__.py
+++ pyBridgeInv/__init__.py
@@ -1,7 +1,39 @@
+from os import read
 from pathlib import Path
 import csv
+import pandas as pd
 
 CODE_DICT_DIR = Path('')
+CODE_TABLE_DELIM = ','
+
+
+def parse_coding_table(table_path):
+    # needs to be able to tables with more than just a - > b mapping
+    # Create nested dicts in some cases?
+    # Nested dictionary approach is one way to do that
+    # Need to handle a n * n table ideally
+    # 
+
+
+    with open(str(table_path)) as handle:
+        reader = csv.reader(handle, delimter=CODE_TABLE_DELIM)
+        header = next(reader)
+        rows = [r for r in reader]
+        coding_dict = {}
+        #p.loc['N']['Description']  This would be used for the most basic ones
+        # where N is the code number
+        # pd.read_csv(csv.path, index_col = 0)
+        # to do this and if there were extra columns you dont have to worry about weird dictionary
+        # structure just overwrite the class lookup when you need to use it
+        if len(header) > 2:  # this is a complex table required a nested dict the first item should be the value in the data
+            # each additional column becomes its own subdictionary
+            # might just be better to read tables as pandas dataframes which would allow
+            # for the "2d aspect of some of the tables that arent just basic lookups"
+
+            
+
+
+
 
 def create_code_dicts():
     code_dict = {}
